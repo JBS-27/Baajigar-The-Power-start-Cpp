0ce68e81edd94b54f170ccb8c804b36bf000782a
JBS-27 <singharshll72@gmail.com>
2026-05-20 23:55:43 +0530

    Add files via upload

---
+++ selection.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> selectionSort(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            int small = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[small] >= nums[j])
+                {
+                    small = j;
+                }
+            }
+
+            if (small != i)
+                swap(nums[small], nums[i]);
+        }
+
+        return nums;
+    }
+};
